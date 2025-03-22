c1b22cadc80e9066d67d7689a13e815312cfd9a6
James <jamesgwhit@gmail.com>
2025-03-22 01:07:14 -0400

    log in

--- api/index.py
+++ api/index.py
@@ -20,24 +20,79 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-class User(BaseModel):
+class CreateUser(BaseModel):
     email: str
     password: str
     firstName: str
     lastName: str
 
 
+class LoginUser(BaseModel):
+    email: str
+    password: str
+
+
 @app.post("/api/py/create-user")
-async def hello_fast_api(user: User):
+async def create_user(user: CreateUser):
     try:
+        # Email taken
+        if await prisma.user.query_raw(
+            'SELECT * FROM "User" WHERE "email" = $1', user.email
+        ):
+            # Return 404
+            return {"status": 404}
+
+        # Email doesn't have @
+        if "@" not in user.email:
+            # Return 405
+            return {"status": 405}
+
+        # Password length < 8
+        if len(user.password) < 8:
+            # Return 406
+            return {"status": 406}
+
+        # First name length < 1
+        if len(user.firstName) < 1:
+            # Return 407
+            return {"status": 407}
+
+        # Last name length < 1
+        if len(user.lastName) < 1:
+            # Return 408
+            return {"status": 408}
+
+        # Create user
         await prisma.user.create(
             data={
                 "email": user.email,
-                "password": user.password,  # Ensure you hash the password in production!
+                "password": user.password,
                 "firstName": user.firstName,
                 "lastName": user.lastName,
             }
         )
+        # Return 200
+        return {"status": 200}
     except Exception as error:
         print(error)
-    return {"message": "Hello from FastAPI"}
+        # Return 400
+        return {"status": 400}
+
+
+@app.post("/api/py/login-user")
+async def create_user(user: LoginUser):
+    try:
+        # User not found
+        if not await prisma.user.query_raw(
+            'SELECT * FROM "User" WHERE "email" = $1 AND "password" = $2',
+            user.email,
+            user.password,
+        ):
+            # Return 404
+            return {"status": 404}
+        # Return 200
+        return {"status": 200}
+    except Exception as error:
+        print(error)
+        # Return 400
+        return {"status": 400}
