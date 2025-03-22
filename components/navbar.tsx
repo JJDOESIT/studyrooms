0ebc5542ad171ef84ed796206c2f82eb2f97cd89
h3nry <h3nry.pas@gmail.com>
2025-03-22 00:00:09 -0400

    Added gold

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
 
 
 export default function Navbar() {
-    return <div className="w-full h-[8vh] flex-row items-center flex fixed top-0 left-0 bg-pacificCyan border-b-2 border-black">
+    return <div className="w-full h-[8vh] flex-row items-center flex fixed top-0 left-0 bg-oldGold border-b-2 border-black">
         <div>
-            <h1 className="font-montserrat text-3xl text-black p-10 font-bold">Studyrooms</h1>
+            <h1 className="font-montserrat text-3xl text-black p-10 font-bold">studyrooms</h1>
         </div>
     </div>
 }
